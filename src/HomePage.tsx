f87cb460dfd7ceb23d4d479c165221a0fa05fca6
OscarG <oscar.gamez88@gmail.com>
2024-03-28 09:43:25 -0700

    landing page work

--- src/HomePage.tsx
+++ src/HomePage.tsx
@@ -1,4 +1,11 @@
 export default function HomePage() {
     return (
-        <h1>test homepage</h1>
+        <>
+            <h1>test homepage</h1>
+            <img
+                alt="profile"
+                src="https://media.licdn.com/dms/image/C5603AQGjSlrn_aPRpQ/profile-displayphoto-shrink_800_800/0/1559333015269?e=1717027200&v=beta&t=NW1fims2R9eHULQL8mzDxNtqMMg6DGVTkxIN2fJHqqc"
+                className="justify-center rounded-full"
+                />
+        </>
     )}
