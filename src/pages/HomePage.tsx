9e98b01076ecca01ab9756afb0f6515db90496da
OscarG <oscar.gamez88@gmail.com>
2024-04-05 13:13:52 -0700

    styling on mainpage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -6,8 +6,8 @@ import { Link } from 'react-router-dom';
 export default function HomePage() {
     return (
         <>
-            <div className="flex flex-col items-center gap-5 mt-5">
-                <h1 className="justify-center">Oscar Gamez</h1>
+            <div className="flex flex-col items-center gap-5 mt-10">
+                <h1 className="justify-center text-4xl">Oscar Gamez</h1>
                 <img
                     alt="profile"
                     src="https://media.licdn.com/dms/image/C5603AQGjSlrn_aPRpQ/profile-displayphoto-shrink_800_800/0/1559333015269?e=1717027200&v=beta&t=NW1fims2R9eHULQL8mzDxNtqMMg6DGVTkxIN2fJHqqc"
