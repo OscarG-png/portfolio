902230ef6f28cf215b5f1785d8953c466ff818d3
OscarG <oscar.gamez88@gmail.com>
2024-04-05 12:40:47 -0700

    adding darkmode

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,22 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import NavMenu from './NavMenu';
 import HomePage from './pages/HomePage';
 import AboutMe from './pages/Aboutme';
+import { ThemeProvider } from '@/components/theme-provider.tsx';
 
 function App() {
     return (
         <>
-            <NavMenu />
-            <div className="container mx-auto">
-                <BrowserRouter>
-                    <Routes>
-                        <Route path="/" element={<HomePage />} />
-                        <Route path="/aboutme" element={<AboutMe />} />
-                    </Routes>
-                </BrowserRouter>
-            </div>
+            <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
+                <NavMenu />
+                <div className="container mx-auto">
+                    <BrowserRouter>
+                        <Routes>
+                            <Route path="/" element={<HomePage />} />
+                            <Route path="/aboutme" element={<AboutMe />} />
+                        </Routes>
+                    </BrowserRouter>
+                </div>
+            </ThemeProvider>
         </>
     );
 }
