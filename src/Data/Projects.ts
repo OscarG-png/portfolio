63d9edc861ca66fae18b90dd3a970a109f953e6c
OscarG <oscar.gamez88@gmail.com>
2024-04-08 16:20:50 -0700

    projects list css

--- src/Data/Projects.ts
+++ src/Data/Projects.ts
@@ -1,14 +1,28 @@
+// Storing my projects data here instead of a database for now.
+
 export const projects = [
     {
         "id": 1,
+        "name": "ShortShort Stories",
+        "link": "https://gitlab.com/shortshort/shortshortv2",
+        "description": "A social media platform for sharing short stories. Built with React, FastAPI, Websockets, and PostgreSQL."
+    },
+    {
+        "id": 2,
         "name": "Vehimanage",
         "link": "https://gitlab.com/OscarG/vehimanage",
         "description": "A vehicle sales and service management system. Built with React, Django, and PostgreSQL."
     },
     {
-        "id": 2,
+        "id": 3,
         "name": "Level up Reviews",
         "link": "https://gitlab.com/pixel-pioneers/level-up-reviews",
         "description": "A user driven video game review site. Built with React, FastAPI, and PostgreSQL."
+    },
+    {
+        "id": 4,
+        "name": "Taskforge",
+        "link": "https://gitlab.com/OscarG/TaskForge",
+        "description": "A task management system. Built with HTML and Django. This was the first project I ever built."
     }
 ]
--- src/pages/ProjectsPage.tsx
+++ src/pages/ProjectsPage.tsx
@@ -12,7 +12,12 @@ export default function Projects() {
         <div className="flex flex-col items-center justify-center gap-5 mt-10">
             <h1 className="text-4xl">Projects i've worked on</h1>
             {projects.map((project) => (
-                <Accordion key={project.id} type="single" collapsible>
+                <Accordion
+                    key={project.id}
+                    type="single"
+                    collapsible
+                    className="w-72"
+                >
                     <AccordionItem value={project.name}>
                         <AccordionTrigger className="text-2xl">
                             {project.name}
