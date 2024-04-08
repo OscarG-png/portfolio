b603c172a7bc6878e437987b020382de06f71f26
OscarG <oscar.gamez88@gmail.com>
2024-04-08 13:28:21 -0700

    spacing on aboutme page

--- src/pages/Aboutme.tsx
+++ src/pages/Aboutme.tsx
@@ -3,7 +3,7 @@ import { aboutMe } from '../Data/AboutMe';
 export default function AboutMe() {
     return (
         <>
-            <div className="flex flex-col items-center justify-center h-screen">
+            <div className="flex flex-col items-center justify-center h-screen gap-5">
                 <h1 className="text-4xl">About me</h1>
                 <p>{aboutMe}</p>
             </div>
