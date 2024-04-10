e006df410d8dedc418ecab16089cec2e3cb870ac
OscarG <oscar.gamez88@gmail.com>
2024-04-10 10:26:43 -0700

    fixing navmenu for mobile

--- src/NavMenu.tsx
+++ src/NavMenu.tsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 export default function NavMenu() {
     return (
         <nav>
-            <div className="flex flex-row gap-5">
+            <div className="flex flex-row gap-5 overflow-x-auto sm:overflow-x-visible whitespace-nowrap">
                 <Link to="/">Home</Link>
                 <Link to="/aboutme">About Me</Link>
                 <Link to="/projects">Projects</Link>
