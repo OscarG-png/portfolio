e261aeaa0a9d5a8a8d8a51f113b7b2790fc7a29a
OscarG <oscar.gamez88@gmail.com>
2024-07-01 11:16:22 -0700

    moved portfolio to vercel

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,12 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import NavMenu from './NavMenu';
-import HomePage from './pages/HomePage';
-import AboutMe from './pages/Aboutme';
-import Projects from './pages/ProjectsPage';
-import { ThemeProvider } from '@/components/theme-provider.tsx';
-import { ModeToggle } from '@/components/mode-toggle';
-import { AnimatePresence } from 'framer-motion';
-
+import { Button } from '@/components/ui/button';
 function App() {
     return (
-        <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
-            <Router basename="/portfolio">
-                <div className="flex flex-row justify-between gap-2 mx-5 my-5">
-                    <NavMenu />
-                    <ModeToggle />
-                </div>
-                <div className="container">
-                    <AnimatePresence mode={'wait'}>
-                        <Routes>
-                            <Route path="/" element={<HomePage />} />
-                            <Route path="/aboutme" element={<AboutMe />} />
-                            <Route path="/projects" element={<Projects />} />
-                        </Routes>
-                    </AnimatePresence>
-                </div>
-            </Router>
-        </ThemeProvider>
+        <div className="container flex flex-col items-center justify-center min-h-screen">
+            <h1 className="text-center">Portfolio has been moved</h1>
+            <a href="https://portfoliov2-steel.vercel.app/">
+                <Button>Go to new Portfolio</Button>
+            </a>
+        </div>
     );
 }
 
